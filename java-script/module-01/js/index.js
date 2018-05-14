841e534ad30cf6021092bbca04e266697be880f5
Yevhen P <pikalovyevhen@gmail.com>
2018-05-14 22:25:09 +0200

    Prettier edit

--- java-script/module-01/js/index.js
+++ java-script/module-01/js/index.js
@@ -1,59 +1,67 @@
 'use strict'
-let sharm = 15;
+
 let hugarda = 25;
+let sharm = 15;
 let taba = 6;
+
+//input
 
 let userInput = prompt('For how many people is reservation?', 2);
 //input check
+
 if (userInput < 1 && parseInt(userInput) !== userInput) {
-    alert('Ooops! Wrong input - pleace try again')
+    alert('Ooops! Wrong input - pleace try again');
 }
+
 //taba
-else {
-    if (taba >= userInput) {
-        let isTaba = confirm('We found places in Taba group. Would you like to join Taba trip?')
-        if (isTaba === true) {
-            taba = taba - userInput
-            alert('Have fun in Taba!')
-            console.log('Free places in Taba: ', taba)
+
+    else {
+        if (taba >= userInput) {
+            let isTaba = confirm('We found places in Taba group. Would you like to join Taba trip?');
+            if (isTaba === true) {
+                taba = taba - userInput
+                alert('Have fun in Taba!');
+                console.log('Free places in Taba: ', taba);
+            }
+
+            else {
+                alert('We are very sorry. Hope to see you soon!');
+            }
         }
-        
-        else {
-            alert('We are very sorry. Hope to see you soon!')
-        }
-    }
+
 //sharm
+
     else if (sharm >= userInput) {
-        let isSharm = confirm('Do you want to have fun in Sharm?')
+        let isSharm = confirm('Do you want to have fun in Sharm?');
         if (isSharm = true) {
             sharm = sharm - userInput
-            alert('Congrat\'s your trip is booked!')
-            console.log('Free places in Sharm: ', sharm);
+            alert('Congrat\'s your trip is booked!');
+            console.log('Free places in Sharm: ', sharm)
         }
-        
+
         else {
-            alert('We are very sorry. Hope to see you soon!')
+            alert('We are very sorry. Hope to see you soon!');
         }
     }
-    //hugarda
+
+//hugarda
+
     else if (hugarda >= userInput) {
-        let isHugarda = confirm('We found places in Hugarda group. Would you like to join Hugarda trip?')
+        let isHugarda = confirm('We found places in Hugarda group. Would you like to join Hugarda trip?');
         if (isHugarda === true) {
             hugarda = hugarda - userInput
-            alert('Have fun in Hugarda!')
-            console.log('Free places in Hugarda: ', hugarda)
+            alert('Have fun in Hugarda!');
+            console.log('Free places in Hugarda: ', hugarda);
         }
-        
-    else {
-            alert('We are very sorry. Hope to see you soon!')
+
+        else {
+            alert('We are very sorry. Hope to see you soon!');
         }
     }
 
-    //hugarda
+//other
 
-
-    //other
     else {
-        alert('Sorry there are no enough availeble places. Try other date!')
+        alert('Sorry there are no enough availeble places. Try other date!');
     }
-};
+}
