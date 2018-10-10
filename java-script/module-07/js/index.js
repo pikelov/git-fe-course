08177b57d24cf0cdc8788871d3c8a9844d4483c8
Yevhen Pikalov <pikalovyevhen@gmail.com>
2018-10-10 15:21:25 +0200

    fix fn createCards to .map method

--- java-script/module-07/js/index.js
+++ java-script/module-07/js/index.js
@@ -10,88 +10,84 @@
 
 const posts = [
   {
-    img: "https://placeimg.com/400/150/arch",
-    title: "Post title 1",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+    img: 'https://placeimg.com/400/150/arch',
+    title: 'Post title 1',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!',
     link: 'link-1.com'
   },
   {
-    img: "https://placeimg.com/400/150/nature",
-    title: "Post title 2",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+    img: 'https://placeimg.com/400/150/nature',
+    title: 'Post title 2',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!',
     link: 'link-2.com'
   },
   {
-    img: "https://placeimg.com/400/150/arch",
-    title: "Post title 3",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+    img: 'https://placeimg.com/400/150/arch',
+    title: 'Post title 3',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!',
     link: 'link-3.com'
   }
 ];
 
-/*
-  функция createPostCard(), которая 
-  создает и возвращает DOM-узел карточки поста.
-  
-  Разметка с классами есть на вкладке HTML.
-  Стили на вкладке CSS.
-*/
-const root = document.querySelector("#root");
+const root = document.querySelector('#root');
 
-// const objPost = {
-//     img: "https://placeimg.com/400/150/arch",
-//     title: "Post title 1",
-//     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
-//     link: 'link-1.com'
-//   };
-
-// const newPost = createPostCard(objPost);
-// root.appendChild(newPost);
+createCards(posts);
 
 function createButton({ text = 'Read more', className = '', link = '#' }) {
-  const btn = document.createElement("a");
+  const btn = document.createElement('a');
   btn.textContent = text;
   btn.classList.add(className);
-  btn.setAttribute('href', link)
+  btn.setAttribute('href', link);
 
   return btn;
-};
+}
 
-function createPostCard({img = '',title = 'newTitle', text = '', link = '' }) {
+function createPostCard({
+  img = '',
+  title = 'newTitle',
+  text = '',
+  link = ''
+}) {
   const postWrapper = document.createElement('div');
-  const postImg = document.createElement('img')
+  const postImg = document.createElement('img');
   const postTitle = document.createElement('h2');
-  const postText = document.createElement('p')
-  const bttn = createButton({text: 'Read more...', className: 'button', link: link});
-  
+  const postText = document.createElement('p');
+  const bttn = createButton({
+    text: 'Read more...',
+    className: 'button',
+    link: link
+  });
+
   postWrapper.classList.add('post');
   postImg.classList.add('post__image');
   postTitle.classList.add('post__title');
-  postText.classList.add('post__text')
-  
+  postText.classList.add('post__text');
+
   postImg.setAttribute('src', img);
-  postImg.setAttribute('alt', 'post image')
-  
+  postImg.setAttribute('alt', 'post image');
+
   postText.textContent = text;
   postTitle.textContent = title;
   postWrapper.append(postImg, postTitle, postText, bttn);
-  
+
   return postWrapper;
-};
+}
 
 function createCards(postsArr) {
-const postsEls = [];
-    postsArr.forEach(item => {
-    const post = createPostCard({
-        img: item.img,
-        title: item.title,
-        text: item.text,
-        link: item.link
-    });
-    postsEls.push(post)
-})
-return root.append(...postsEls);
-} 
+  const postsEls = postsArr.map(postItem => createPostCard(postItem));
 
-
-createCards(posts);
+  // const postsEls = [];
+  // postsArr.forEach(item => {
+  //   const post = createPostCard({
+  //     img: item.img,
+  //     title: item.title,
+  //     text: item.text,
+  //     link: item.link
+  //   });
+  //   postsEls.push(post);
+  // });
+  return root.append(...postsEls);
+}
