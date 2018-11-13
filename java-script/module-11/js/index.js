8f641f28bbe2e776a08d91f00263215ec972a0da
Yevhen Pikalov <pikalovyevhen@gmail.com>
2018-11-13 16:11:34 +0100

    file add

---
+++ java-script/module-11/js/index.js
@@ -0,0 +1,93 @@
+const laptops = [
+  {
+    size: 13,
+    color: 'white',
+    price: 28000,
+    release_date: 2015,
+    name: 'Macbook Air White 13"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 13,
+    color: 'gray',
+    price: 32000,
+    release_date: 2016,
+    name: 'Macbook Air Gray 13"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 13,
+    color: 'black',
+    price: 35000,
+    release_date: 2017,
+    name: 'Macbook Air Black 13"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 15,
+    color: 'white',
+    price: 45000,
+    release_date: 2015,
+    name: 'Macbook Air White 15"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 15,
+    color: 'gray',
+    price: 55000,
+    release_date: 2016,
+    name: 'Macbook Pro Gray 15"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 15,
+    color: 'black',
+    price: 45000,
+    release_date: 2017,
+    name: 'Macbook Pro Black 15"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 17,
+    color: 'white',
+    price: 65000,
+    release_date: 2015,
+    name: 'Macbook Air White 17"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 17,
+    color: 'gray',
+    price: 75000,
+    release_date: 2016,
+    name: 'Macbook Pro Gray 17"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 17,
+    color: 'black',
+    price: 80000,
+    release_date: 2017,
+    name: 'Macbook Pro Black 17"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+];
+
